6eb35a873d297e803a267e0d45db4effc474827d
mwz73trz <michaelzura@bellsouth.net>
2023-06-16 15:14:34 -0400

    complete

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="box">Hover me</div>
+      <hr />
+      <div id="ball-container">
+        <div id="ball" />
+      </div>
+      <hr />
+      <div id="sprite-animation" />
+    </div>
+  );
+}
+
+export default App;
